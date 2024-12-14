f86b50bb662e23dce4ee2ae8bd1f726064785a89
HerbertKayky <herbertkayky9@gmail.com>
2024-12-13 22:11:59 -0300

    Adiciona o componente <Projects /> à página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from "@/components/about";
 import { Landing } from "@/components/landing";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <>
       <Landing />
       <About />
+      <Projects />
     </>
   );
 }
