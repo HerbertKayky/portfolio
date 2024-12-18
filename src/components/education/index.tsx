be86b82fb647a3dbb113c731bde3ac69157c24f3
HerbertKayky <herbertkayky9@gmail.com>
2024-12-17 23:44:57 -0300

    Cria componente <Education />

---
+++ src/components/education/index.tsx
@@ -0,0 +1,28 @@
+export function Education() {
+  return (
+    <section
+      id="education"
+      className="bg-[#212121] text-[#BEB7B7] pt-2 min-h-screen"
+    >
+      <div className="container mx-auto px-5">
+        <h2 className="text-4xl md:text-5xl font-bold text-[#F56539] mb-8 text-center">
+          Formação Acadêmica
+        </h2>
+        <div className="bg-[#333333] rounded-lg shadow-lg p-6 text-center">
+          <h3 className="text-2xl font-bold text-[#EAEAEA] mb-4">
+            Análise e Desenvolvimento de Sistemas
+          </h3>
+          <p className="text-lg leading-7">
+            Formado pela{" "}
+            <span className="text-[#F56539] font-semibold">
+              Uninassau - Caruaru
+            </span>
+            . Durante minha graduação, adquiri habilidades em desenvolvimento de
+            sistemas, resolução de problemas e inovação tecnológica, me
+            capacitando para atuar na área de tecnologia com excelência.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
