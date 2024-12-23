90bca1a6e160122e1c2b826f4b2e3e24cde7aa26
HerbertKayky <herbertkayky9@gmail.com>
2024-12-22 21:22:20 -0300

    Adiciona componente <Skills /> à página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { About } from "@/components/about";
 import { Education } from "@/components/education";
 import { Landing } from "@/components/landing";
 import Projects from "@/components/projects";
+import { Skills } from "@/components/skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Projects />
       <Education />
+      <Skills />
     </>
   );
 }
