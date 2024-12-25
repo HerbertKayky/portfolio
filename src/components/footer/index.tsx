acb27aa5ef58c009c973c3050114ac26e54e801d
HerbertKayky <herbertkayky9@gmail.com>
2024-12-24 21:05:24 -0300

    Cria componente <Footer />

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="bg-[#212121] text-[#BEB7B7] pt-5 text-center">
+      <p className="text-xl">
+        Desenvolvido com <span className="text-[#F56539]">&lt;3</span> por
+        Herbert Kayky
+      </p>
+    </footer>
+  );
+}
