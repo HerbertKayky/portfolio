263a6575cd8b496b1027e51bee94a4a0e736ace1
HerbertKayky <herbertkayky9@gmail.com>
2024-12-29 11:30:36 -0300

    Adiciona língua inglesa ao componente <Education />

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -1,15 +1,21 @@
+"use client";
 import Image from "next/image";
 import studyImg from "/public/study.png";
 import motarboard from "/public/207714.png";
+import { useLanguage } from "@/context/LanguageContext";
 
 export function Education() {
+  const { language } = useLanguage();
+
   return (
     <section
       id="education"
-      className="bg-[#212121] text-[#BEB7B7] py-20 min-h-screen"
+      className="bg-[#212121] text-[#BEB7B7] min-h-screen"
     >
       <div className="container mx-auto px-5">
-        <h2 className="text-5xl font-bold text-[#F56539] mb-12">Formação</h2>
+        <h2 className="text-5xl font-bold text-[#F56539] mb-12">
+          {language === "pt" ? "Formação" : "Education"}
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           <div className="md:col-span-2 space-y-8">
             <div className="flex items-center bg-[#613528] rounded-3xl shadow-lg p-6 hover:bg-[#8B422C] transition-all">
@@ -28,7 +34,9 @@ export function Education() {
                   2022 - 2024
                 </p>
                 <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  Graduação em Análise e Desenvolvimento de Sistemas
+                  {language === "pt"
+                    ? "Graduação em Análise e Desenvolvimento de Sistemas"
+                    : "Degree in Systems Analysis and Development"}
                 </h3>
                 <p className="text-lg">UNINASSAU - Caruaru</p>
               </div>
@@ -48,7 +56,9 @@ export function Education() {
               <div>
                 <p className="text-sm font-bold text-[#F56539] mb-2">2024</p>
                 <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  NextJS do Zero ao Avançado
+                  {language === "pt"
+                    ? "NextJS do zero à maestria"
+                    : "NextJS from zero to mastery"}
                 </h3>
                 <p className="text-lg">
                   Udemy - Mateus Fragua (Sujeito Programador)
@@ -70,7 +80,9 @@ export function Education() {
               <div>
                 <p className="text-sm font-bold text-[#F56539] mb-2">2023</p>
                 <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  React do Zero à Maestria
+                  {language === "pt"
+                    ? "React do zero à maestria"
+                    : "React from zero to mastery"}
                 </h3>
                 <p className="text-lg">
                   Udemy - Matheus Battisti (Hora de Codar)
