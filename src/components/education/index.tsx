545b33c0324a39e00a398bd10a452d798810d748
HerbertKayky <herbertkayky9@gmail.com>
2025-01-07 22:46:14 -0300

    Adiciona animação com framer motion ao componente <Education />

--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -3,9 +3,15 @@ import Image from "next/image";
 import studyImg from "/public/study.png";
 import motarboard from "/public/207714.png";
 import { useLanguage } from "@/context/LanguageContext";
+import { motion } from "framer-motion";
 
 export function Education() {
   const { language } = useLanguage();
+
+  const courseVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
 
   return (
     <section
@@ -13,82 +19,67 @@ export function Education() {
       className="bg-[#212121] text-[#BEB7B7] min-h-screen"
     >
       <div className="container mx-auto px-5">
-        <h2 className="text-5xl font-bold text-[#F56539] mb-12">
+        <h2 className="text-5xl font-bold text-[#F56539] mb-12 lg:pt-0 pt-12">
           {language === "pt" ? "Formação" : "Education"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
           <div className="md:col-span-2 space-y-8">
-            <div className="flex items-center bg-[#613528] rounded-3xl shadow-lg p-6 hover:bg-[#8B422C] transition-all">
-              <div className="flex-shrink-0 mr-6">
-                <div className="w-16 h-16 bg-[#E57C58] flex justify-center items-center rounded-full">
-                  <Image
-                    src={motarboard}
-                    width={50}
-                    height={50}
-                    alt="Ícone de formação"
-                  />
+            {[
+              {
+                year: "2022 - 2024",
+                title: {
+                  pt: "Graduação em Análise e Desenvolvimento de Sistemas",
+                  en: "Degree in Systems Analysis and Development",
+                },
+                location: "UNINASSAU - Caruaru",
+              },
+              {
+                year: "2024",
+                title: {
+                  pt: "NextJS do zero à maestria",
+                  en: "NextJS from zero to mastery",
+                },
+                location: "Udemy - Mateus Fragua (Sujeito Programador)",
+              },
+              {
+                year: "2023",
+                title: {
+                  pt: "React do zero à maestria",
+                  en: "React from zero to mastery",
+                },
+                location: "Udemy - Matheus Battisti (Hora de Codar)",
+              },
+            ].map((course, index) => (
+              <motion.div
+                key={index}
+                className="flex items-center bg-[#613528] rounded-3xl shadow-lg p-6 hover:bg-[#8B422C] transition-all"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={courseVariants}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+              >
+                <div className="flex-shrink-0 mr-6">
+                  <div className="w-16 h-16 bg-[#E57C58] flex justify-center items-center rounded-full">
+                    <Image
+                      src={motarboard}
+                      width={50}
+                      height={50}
+                      alt="Ícone de formação"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p className="text-sm font-bold text-[#F56539] mb-2">
-                  2022 - 2024
-                </p>
-                <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  {language === "pt"
-                    ? "Graduação em Análise e Desenvolvimento de Sistemas"
-                    : "Degree in Systems Analysis and Development"}
-                </h3>
-                <p className="text-lg">UNINASSAU - Caruaru</p>
-              </div>
-            </div>
-
-            <div className="flex items-center bg-[#613528] rounded-3xl shadow-lg p-6 hover:bg-[#8B422C] transition-all">
-              <div className="flex-shrink-0 mr-6">
-                <div className="w-16 h-16 bg-[#E57C58] flex justify-center items-center rounded-full">
-                  <Image
-                    src={motarboard}
-                    width={50}
-                    height={50}
-                    alt="Ícone de formação"
-                  />
+                <div>
+                  <p className="text-sm font-bold text-[#F56539] mb-2">
+                    {course.year}
+                  </p>
+                  <h3 className="text-2xl font-bold text-[#EAEAEA]">
+                    {language === "pt" ? course.title.pt : course.title.en}
+                  </h3>
+                  <p className="text-lg">{course.location}</p>
                 </div>
-              </div>
-              <div>
-                <p className="text-sm font-bold text-[#F56539] mb-2">2024</p>
-                <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  {language === "pt"
-                    ? "NextJS do zero à maestria"
-                    : "NextJS from zero to mastery"}
-                </h3>
-                <p className="text-lg">
-                  Udemy - Mateus Fragua (Sujeito Programador)
-                </p>
-              </div>
-            </div>
-
-            <div className="flex items-center bg-[#613528] rounded-3xl shadow-lg p-6 hover:bg-[#8B422C] transition-all">
-              <div className="flex-shrink-0 mr-6">
-                <div className="w-16 h-16 bg-[#E57C58] flex justify-center items-center rounded-full">
-                  <Image
-                    src={motarboard}
-                    width={50}
-                    height={50}
-                    alt="Ícone de formação"
-                  />
-                </div>
-              </div>
-              <div>
-                <p className="text-sm font-bold text-[#F56539] mb-2">2023</p>
-                <h3 className="text-2xl font-bold text-[#EAEAEA]">
-                  {language === "pt"
-                    ? "React do zero à maestria"
-                    : "React from zero to mastery"}
-                </h3>
-                <p className="text-lg">
-                  Udemy - Matheus Battisti (Hora de Codar)
-                </p>
-              </div>
-            </div>
+              </motion.div>
+            ))}
           </div>
 
           <div className="flex justify-center items-center">
