40655f9feb5c3eb6dea28db9a115a30978dfbaf4
HerbertKayky <herbertkayky9@gmail.com>
2025-01-09 21:32:53 -0300

    Adiciona framer motion ao componente <Footer />

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,30 @@
 import { useLanguage } from "@/context/LanguageContext";
+import { motion } from "framer-motion";
 
 export default function Footer() {
   const { language } = useLanguage();
 
+  const footerTextPt = ["Desenvolvido com ", "❤", " por Herbert Kayky"];
+  const footerTextEn = ["Made with ", "❤", " by Herbert Kayky"];
+  const footerText = language === "pt" ? footerTextPt : footerTextEn;
+
   return (
     <footer className="bg-[#212121] text-white pt-10 text-center">
       <p className="text-xl">
-        {language === "pt" ? "Desenvolvido com" : "Made with"}{" "}
-        <span className="text-[#F56539]">❤</span>{" "}
-        {language === "pt" ? "por Herbert Kayky" : "by Herbert Kayky"}
+        {footerText.map((part, index) => (
+          <motion.span
+            key={index}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.2,
+              delay: index * 0.2,
+            }}
+            className={part === "❤" ? "text-[#F56539]" : ""}
+          >
+            {part}
+          </motion.span>
+        ))}
       </p>
     </footer>
   );
